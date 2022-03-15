24c336b5a7ed03d47be5cce80db4ea2e2697bb4e
MarcosTecTrilha <marcos@tectrilha.com.br>
2022-03-14 23:46:50 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <PrimeiroComponente/>
+  <LifeCycle />
+</div>  
+</template>
+
+
+<script>
+   
+  import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+  import LifeCycleVue from "./components/LifeCycle.vue"
+import LifeCycle from "./components/LifeCycle.vue"
+
+  export default {
+    name: 'App',
+    components: {
+    PrimeiroComponente,
+    LifeCycleVue,
+    LifeCycle
+}
+  }
+</script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    }, 
+    created() {
+        setTimeout(() => {
+
+        }, 1000);
+        this.nome = "Marcos"
+    },
+    mounted() {
+        
+        this.nome = 'Pedro'
+    }
+}
+</script>
